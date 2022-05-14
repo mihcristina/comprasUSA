4e2e8eaff31f30209933432bd971cb3e1f2beed5
mihcristina <contato@michellicristina.com.br>
2022-05-14 11:09:09 -0300

    inicial configurations, with storyboard configuration

---
+++ comprasUSA/ShoppingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  comprasUSA
+//
+//  Created by Michelli Cristina de Paulo Lima on 13/05/22.
+//
+
+import UIKit
+
+class ShoppingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
